19d7ffae106fe2f59544537ea05bd6175af4825b
Caitlin Stein <caitlin.teter@gmail.com>
2016-03-07 13:18:26 -0600

    weapons now inherit fromm weapon parent class

--- modern-javascript-developer/quiz/src/weapons.js
+++ modern-javascript-developer/quiz/src/weapons.js
@@ -4,46 +4,46 @@ let helper = require("./helper");
 
 ///Building weapons
 function Weapon() {
-
+  his.damage = helper.randomRange(15, 20);
 }
 
 function Spoon() {
-  this.damage = helper.randomRange(15, 20);
+  this.damage = this.damage + 5;
   this.name = "Spoon";
 }
 Spoon.prototype = new Weapon();
 
 function Wand() {
-  this.damage = helper.randomRange(5, 30);
+  this.damage = this.damage + 3;
   this.name = "Wand";
 }
 Wand.prototype = new Weapon();
 
 function LightSaber() {
-  this.damage = helper.randomRange(20, 30);
+  this.damage = this.damage + 7;
   this.name = "LightSaber";
 }
 LightSaber.prototype = new Weapon();
 
 function Scimitar() {
-  this.damage = helper.randomRange(10, 20);
+  this.damage = this.damage + 2;
   this.name = "Scimitar";
 }
 Scimitar.prototype = new Weapon();
 
 function NailClippers() {
-  this.damage = helper.randomRange(5, 15);
+  this.damage = this.damage - 2;
   this.name = "NailClippers";
 }
 NailClippers.prototype = new Weapon();
 
 function ProtonPack() {
-  this.damage = helper.randomRange(5, 20);
+  this.damage = this.damage + 8;
   this.name = "ProtonPack";
 }
 ProtonPack.prototype = new Weapon();
 
 let weapons = {
   Spoon, Wand, LightSaber, Scimitar, NailClippers, ProtonPack
-}
+};
 module.exports = weapons;
